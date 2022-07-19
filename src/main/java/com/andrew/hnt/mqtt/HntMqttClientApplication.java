7475604c6f7d54bd29aa853c168d7eab2782e50f
andrew <andrew2767@gmail.com>
2022-07-20 00:40:45 +0900

    recommit

---
+++ src/main/java/com/andrew/hnt/mqtt/HntMqttClientApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.hnt.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HntMqttClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HntMqttClientApplication.class, args);
+	}
+
+}
